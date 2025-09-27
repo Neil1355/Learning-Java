51be9db06fe5596f779dc13b813523fcf74138e5
Neil Barot <neilbarot5@gmail.com>
2025-09-27 15:41:26 -0400

    Add files via upload

---
+++ CharArrayExample.java
@@ -0,0 +1,24 @@
+package Array;
+
+/**
+ *
+ * @author xpert infotech
+ */
+public class CharArrayExample {
+	public static void main(String[] args) {
+		char arr[] = new char[5];
+		int i, j;
+		char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+
+		for (i = 0; i < 5; i++) {
+			System.out.print(charArray[i]);
+
+		}
+		for (i = 4; i >=0; i--) {
+			for (j = 0; j <= i; j++) {
+				System.out.print(" "+ charArray[j]);
+			}
+			System.out.println("");
+		}
+	}
+}
