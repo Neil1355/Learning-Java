2e0733bb6f4ac55d33bd79fd48162adcd574f807
Neil Barot <neilbarot5@gmail.com>
2025-10-04 12:49:21 -0400

    Add files via upload

---
+++ HashSetDemo.java
@@ -0,0 +1,19 @@
+package DataStructures_Collections;
+import java.util.*;
+
+
+class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet<String> hs = new HashSet<String>();
+		
+		hs.add("Alpha");
+		hs.add("Beta");
+		hs.add("gamma");
+		hs.add("eta");
+		hs.add("epsilon");
+		hs.add("omega");
+		
+		System.out.println(hs);
+	}
+
+}
