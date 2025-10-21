5d8abdeb72514c3ab1b30659db68f73abafeed6c
Neil Barot <neilbarot5@gmail.com>
2025-10-20 20:03:26 -0400

    Add files via upload

---
+++ fileIO/GFG.java
@@ -0,0 +1,18 @@
+package learning.fileio;
+
+import java.io.*;
+
+public class GFG {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter an integer");
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println("Enter a String");
+        String b = br.readLine();
+
+        System.out.println("You have entered:- " + a + " and name as " + b);
+    }
+}
