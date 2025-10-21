279a4fabdc490fdb8556088f9a885894aa429249
Neil Barot <neilbarot5@gmail.com>
2025-10-20 20:02:55 -0400

    Add files via upload

---
+++ fileIO/ExgrDemo.java
@@ -0,0 +1,54 @@
+package Homework;
+
+import java.util.concurrent.Exchanger;
+
+class ExgrDemo {
+	public static void main(String[] args) {
+		Exchanger<String> exgr = new Exchanger<String>();
+		new Thread(new UseString(exgr)).start();
+		new Thread(new MakeString(exgr)).start();
+	}
+}
+
+class MakeString implements Runnable {
+	Exchanger<String> ex;
+	String str;
+
+	MakeString(Exchanger<String> c) {
+		ex = c;
+		str = new String();
+	}
+
+	public void run() {
+		char ch = 'A';
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 5; j++)
+				str += ch++;
+			try {
+				str = ex.exchange(str);
+			} catch (InterruptedException exc) {
+				System.out.println(exc);
+			}
+		}
+	}
+}
+
+class UseString implements Runnable {
+	Exchanger<String> ex;
+	String str;
+
+	UseString(Exchanger<String> c) {
+		ex = c;
+	}
+
+	public void run() {
+		for (int i = 0; i < 3; i++) {
+			try {
+				str = ex.exchange(new String());
+				System.out.println("Got: " + str);
+			} catch (InterruptedException exc) {
+				System.out.println(exc);
+			}
+		}
+	}
+}
