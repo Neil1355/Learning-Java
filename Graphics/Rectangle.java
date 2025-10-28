952c823e0e2fab199d0cdfff7f2fd9d07379e82d
Neil Barot <neilbarot5@gmail.com>
2025-10-27 23:09:36 -0400

    Add files via upload

---
+++ Graphics/Rectangle.java
@@ -0,0 +1,10 @@
+
+public class Rectangle {
+ public static void main(String args[]) {
+	 int length, breadth, area;
+	 length = 6;
+	 breadth =5;
+	 area =length*breadth;
+	 System.out.println("area Of Rectangle is "+area);
+ }
+}
