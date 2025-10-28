d2564e2fbd5f9583c0a1a6a659bae65a9b50a7e6
Neil Barot <neilbarot5@gmail.com>
2025-10-27 23:09:15 -0400

    Add files via upload

---
+++ Graphics/Boxdemo2.java
@@ -0,0 +1,11 @@
+class Boxdemo2 {
+	public static void main(String[] args) {
+		int Student[] = new int[3];
+		int Exam[] = new int[3];
+		for (int i = 0; i < Student.length; i++) {
+			for (int j = 0; j < Exam.length; j++) {
+				System.out.println("hello");
+			}
+		}
+	}
+}
