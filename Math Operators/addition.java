2b1cccb385452c866330fe13a3b6a5e4cfad29c0
Neil Barot <neilbarot5@gmail.com>
2025-11-03 23:50:53 -0500

    Add files via upload

---
+++ Math Operators/addition.java
@@ -0,0 +1,10 @@
+
+public class addition {
+public static void main(String args[]) {
+		int num1, num2, answer ;
+		num1 = 55;
+		num2 = 45;
+		answer = num1+num2;
+		System.out.println("addition :"+answer);
+}
+}
