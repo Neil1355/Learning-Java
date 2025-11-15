5829f8bdf9a2f0b6769f1449381eecb84722abf2
Neil Barot <neilbarot5@gmail.com>
2025-11-14 20:58:58 -0500

    Add files via upload

---
+++ Mini Projects/Transaction.java
@@ -0,0 +1,19 @@
+
+public class Transaction {
+
+    public void setTransactionDate(String format) {
+    }
+
+    public void setTransactionTime(String format) {
+    }
+
+    public void setAmount(double amount) {
+    }
+
+    public void setKind(String deposit) {
+    }
+
+    public void setBalance(double balance) {
+    }
+
+}
